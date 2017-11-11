a2a2bbb696153e8484fec6707d9bac27f4f7ec17
dengas-khan <32390675+dengas-khan@users.noreply.github.com>
2017-11-11 16:55:10 +0000

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,21 +1,21 @@
 import random
 import player  
 
-def start_turn(player):
-    def gain_devotion():
-        x = input('Enter which devotion to gain: ')
-        elements = [player.devotion1, player.devotion2]
-        if x in elements:
-            player.x = player.x + 1
-            return player.x
-        else:
-            return gain_devotion()
+def gain_devotion(player):
+    x = input('Enter which devotion to gain: ')
+    elements = [player.devotion1, player.devotion2]
+    if x in elements:
+        player.x = player.x + 1
+        return player.x
+    else:
+        return gain_devotion()       
             
 def main():
     x = random.randrange(1, 3)
     if x == 1:
-        start_turn('Player1')
+        y = gain_devotion('Player1')
     elif x == 2:
-        start_turn('Player2')
+        y = gain_devotion('Player2')
+    print(y)    
 
 main() 
